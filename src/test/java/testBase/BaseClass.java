2a994091b90b7ca86a5a7efe150fd0c09e54bf1d
Dnyanesh00123 <dnyaneshg.001@gmail.com>
2026-07-27 19:27:34 +0530

    Fix variable name in execution environment print statement

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -47,7 +47,7 @@ public class BaseClass {
            String executionEnv = p.getProperty("execution_env");
 
        // Print execution environment
-          System.out.println("Execution Environment : " + executionEnv);
+          System.out.println("Execution Environment : " + execution_env);
 		
 		//Remote execution
 		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
